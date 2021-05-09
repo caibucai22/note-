3bc1211e99a9379c78426115a5f8fd4f76542b37
csy <1608088355@qq.com>
2021-05-09 17:35:33 +0800

    实现设置页面

---
+++ app/src/main/java/cn/henu/cs/note/MyBaseAdapter.java
@@ -0,0 +1,55 @@
+package cn.henu.cs.note;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyBaseAdapter extends BaseAdapter {
+
+    private List<Map<String,Object>> data;
+    private Context mcontext;
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ListView listView = new ListView(mcontext);
+        return listView;
+    }
+
+    public MyBaseAdapter(Context mContext,List<Map<String,Object>> data){
+        super();
+        this.data = data;
+        this.mcontext = mContext;
+    }
+
+    private List<Map<String,Object>> getData(){
+        List<Map<String,Object>> list = new ArrayList<Map<String, Object>>();
+        Map<String,Object> map = new HashMap<String, Object>();
+
+
+        return list;
+    }
+}
